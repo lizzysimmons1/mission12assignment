ebc52bb2bca0e6bf747f9963bcc3d2d51cad1c17
lizzysimmons1 <153117350+lizzysimmons1@users.noreply.github.com>
2025-04-02 14:36:13 -0600

    fixed url

--- backend/mission11.API/Program.cs
+++ backend/mission11.API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<BookDbContext>(options =>
 builder.Services.AddCors(options =>
         options.AddPolicy("AllowReactApp",
     policy => {
-        policy.WithOrigins("http://localhost:3000")
+        policy.AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader();
     }));
